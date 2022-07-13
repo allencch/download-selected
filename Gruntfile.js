0c391990318fe67ee9b07e37f71f14c1089fe315
Allen <allencch@hotmail.com>
2022-07-14 01:19:22 +0800

    Upgrade to manifest v3

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,20 +4,12 @@ module.exports = grunt => {
       ui: {
         files: ['src/ui/**/*.js'],
         tasks: ['concat']
-      },
-      background: {
-        files: ['src/background/**/*.js'],
-        tasks: ['concat']
       }
     },
     concat: {
       ui: {
         src: ['src/ui/lib/**/*.js', 'src/ui/index.js'],
         dest: 'src/ui.js'
-      },
-      background: {
-        src: ['src/background/lib/**/*.js', 'src/background/index.js'],
-        dest: 'src/background.js'
       }
     }
   });
--- src/background/lib/download.js
+++ src/background/lib/download.js
@@ -1,119 +1,108 @@
-/* global chrome */
-(() => {
-  const NUM_JOB = 2;
-  var browser = browser || chrome;
+import { fetchByXhr } from './fetchByXhr';
+import JSZip from './jszip';
 
-  // Setup
-  window.DSB = window.DSB || {};
-  const { DSB } = window;
+const NUM_JOB = 2;
+var browser = browser || chrome;
 
-  /**************
-   * Downloads
-   **************/
+const notifyTab = (tabId, message) => {
+  browser.tabs.sendMessage(tabId, {
+    type: 'WRITE_MESSAGE',
+    message
+  });
+};
 
-  const notifyTab = (tabId, message) => {
-    browser.tabs.sendMessage(tabId, {
-      type: 'WRITE_MESSAGE',
-      message
-    });
-  };
+const getFileExtension = (url) => {
+  const matches = url.match(/\.(\w+)\b/g);
+  if (!matches) {
+    return null;
+  }
+  const lastMatch = matches[matches.length - 1];
+  return lastMatch.substring(1);
+};
 
-  const getFileExtension = (url) => {
-    const matches = url.match(/\.(\w+)\b/g);
-    if (!matches) {
-      return null;
-    }
-    const lastMatch = matches[matches.length - 1];
-    return lastMatch.substring(1);
-  };
-
-  // @deprecated
-  const _downloadFile = (link, onError) => {
-    return new Promise((resolve, reject) => {
-      return fetch(link.url, {
-        referrer: link.referrer
-      }).then(response => {
-        if (response.ok) {
-          const name = `${link.text}.${getFileExtension(link.url)}`;
-          return response
-            .arrayBuffer()
-            .then(data => {
-              resolve({
-                ...link,
-                name,
-                data
-              });
-            });
-        } else {
-          return onError(reject);
-        }
-      });
-    });
-  };
-
-  const downloadFile = (link, onError) => {
-    const { fetchByXhr } = DSB;
-    return new Promise((resolve, reject) => {
-      return fetchByXhr(link.url, {
-        referrer: link.referrer
-      }).then(arrayBuffer => {
+// @deprecated
+const _downloadFile = (link, onError) => {
+  return new Promise((resolve, reject) => {
+    return fetch(link.url, {
+      referrer: link.referrer
+    }).then(response => {
+      if (response.ok) {
         const name = `${link.text}.${getFileExtension(link.url)}`;
-        return resolve({
-          ...link,
-          name,
-          data: arrayBuffer
-        });
-      }).catch(() => {
+        return response
+          .arrayBuffer()
+          .then(data => {
+            resolve({
+              ...link,
+              name,
+              data
+            });
+          });
+      } else {
         return onError(reject);
+      }
+    });
+  });
+};
+
+const downloadFile = (link, onError) => {
+  return new Promise((resolve, reject) => {
+    return fetchByXhr(link.url, {
+      referrer: link.referrer
+    }).then(arrayBuffer => {
+      const name = `${link.text}.${getFileExtension(link.url)}`;
+      return resolve({
+        ...link,
+        name,
+        data: arrayBuffer
       });
+    }).catch(() => {
+      return onError(reject);
     });
-  };
+  });
+};
 
-  const createZip = (fetchedData, tabId) => {
-    const zip = JSZip();
+const createZip = (fetchedData, tabId) => {
+  const zip = JSZip();
 
-    for (let i = 0; i < fetchedData.length; i++) {
-      const link = fetchedData[i];
-      zip.file(link.name, link.data);
-    }
+  for (let i = 0; i < fetchedData.length; i++) {
+    const link = fetchedData[i];
+    zip.file(link.name, link.data);
+  }
 
-    const result = zip.generateAsync({ type: 'blob' });
-    notifyTab(tabId, 'Zipping...');
-    result.then(content => {
-      notifyTab(tabId, 'Done');
+  const result = zip.generateAsync({ type: 'blob' });
+  notifyTab(tabId, 'Zipping...');
+  result.then(content => {
+    notifyTab(tabId, 'Done');
 
-      // Use FileSaver.saveAs. Firefox is not able to
-      // download from blob: protocol
-      window.saveAs(content, 'download.zip');
-    }).catch(err => {
-      notifyTab(tabId, `Error: ${err}`);
-      console.log(err);
+    // Use FileSaver.saveAs. Firefox is not able to
+    // download from blob: protocol
+    window.saveAs(content, 'download.zip');
+  }).catch(err => {
+    notifyTab(tabId, `Error: ${err}`);
+    console.log(err);
+  });
+};
+
+
+export const recursiveFetch = (fetchedData = [], links, count, { tabId }) => {
+  if (count >= links.length) {
+    notifyTab(tabId, 'Finish');
+    createZip(fetchedData, tabId);
+    return;
+  }
+
+  const two = links.slice(count, count + NUM_JOB);
+  const promises = two.map(link => downloadFile(link, (reject) => {
+    const message = `Fail download ${link.url}`;
+    notifyTab(tabId, message);
+    return reject(new Error(message));
+  }));
+
+  Promise.all(promises).then(list => {
+    list.forEach(link => {
+      notifyTab(tabId, `${link.text} downloaded`);
+      fetchedData.push(link);
     });
-  };
-
-
-  const recursiveFetch = (fetchedData = [], links, count, { tabId }) => {
-    if (count >= links.length) {
-      notifyTab(tabId, 'Finish');
-      createZip(fetchedData, tabId);
-      return;
-    }
-
-    const two = links.slice(count, count + NUM_JOB);
-    const promises = two.map(link => downloadFile(link, (reject) => {
-      const message = `Fail download ${link.url}`;
-      notifyTab(tabId, message);
-      return reject(new Error(message));
-    }));
-
-    Promise.all(promises).then(list => {
-      list.forEach(link => {
-        notifyTab(tabId, `${link.text} downloaded`);
-        fetchedData.push(link);
-      });
-      recursiveFetch(fetchedData, links, count + NUM_JOB, { tabId });
-    });
-  };
-
-  DSB.recursiveFetch = recursiveFetch;
-})();
+    recursiveFetch(fetchedData, links, count + NUM_JOB, { tabId });
+  });
+};
